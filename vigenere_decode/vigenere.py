229806ee546667938234151b6a69f7d15a409c60
enabledornot <102371948+enabledornot@users.noreply.github.com>
2024-09-02 19:41:03 -0400

    added the cesar and offset options

--- vigenere_decode/vigenere.py
+++ vigenere_decode/vigenere.py
@@ -17,31 +17,41 @@ def handle_input(input):
         return input
 # codeword - the word which the message was encoded with
 # e_message - the message which has been encoded
-def v_decode(codeword, e_message):
+def v_decode(codeword, e_message, cesar=0, word_off=0):
     d_message = ""
     for i in range(len(e_message)):
-        new_num = (char_to_num(e_message[i]) - char_to_num(codeword[i % len(codeword)])) % 26
+        new_num = (char_to_num(e_message[i]) - char_to_num(codeword[(i + word_off) % len(codeword)]) + cesar) % 26
         d_message+=num_to_chr(new_num)
     return d_message
-def v_encode(codeword, d_message):
+def v_encode(codeword, d_message, cesar=0, word_off=0):
     e_message = ""
     for i in range(len(d_message)):
-        new_num = (char_to_num(codeword[i % len(codeword)]) + char_to_num(d_message[i])) % 26
+        new_num = (char_to_num(codeword[(i + word_off) % len(codeword)]) + char_to_num(d_message[i]) + cesar) % 26
         e_message+=num_to_chr(new_num)
     return e_message
+def v_guess_length()
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("invalid command format")
-        print("python vigenere.py <encode:decode> <message_or_filename_of_message> <codeword_or_filename_of_codeword>")
-    else:
+    if len(sys.argv) == 4:
         message = handle_input(sys.argv[2])
         codeword = handle_input(sys.argv[3])
         if sys.argv[1] == 'encode':
+            # for i in range(26):
+            #     print(v_encode(codeword,message,cesar=i))
             result = v_encode(codeword,message)
             print("encoded message: {}".format(result))
         elif sys.argv[1] == 'decode':
+            # for i in range(19):
+            #     print(v_decode(codeword,message,word_off=i))
             result = v_decode(codeword,message)
             print("decoded message: {}".format(result))
         else:
             print("invalid command action")
             print("python vigenere.py <encode:decode> <message_or_filename_of_message> <codeword_or_filename_of_codeword>")
+    elif len(sys.argv) == 3:
+        message = handle_input(sys.argv[2])
+        if sys.argv[1] == 'cw_len':
+            result = v_guess_length(message)
+            print("guessed codeword length: {}".format(result))
+    else:
+        print("invalid command format")
+        print("python vigenere.py <encode:decode> <message_or_filename_of_message> <codeword_or_filename_of_codeword>")
