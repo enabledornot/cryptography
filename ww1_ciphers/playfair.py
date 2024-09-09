bb30cac8b5fd9c71e82dd19c2554d647dfe2003b
enabledornot <102371948+enabledornot@users.noreply.github.com>
2024-09-08 21:29:13 -0400

    moved grid code to its own object

--- ww1_ciphers/playfair.py
+++ ww1_ciphers/playfair.py
@@ -2,68 +2,49 @@ import string
 import textwrap
 import sys
 from tools import *
-def playfair_grid(codeword):
-    codeword = clean_text(codeword)
-    grid = []
-    for c in codeword:
-        if c not in grid:
-            grid.append(c)
-    for c in string.ascii_lowercase:
-        if c == 'j':
-            continue
-        if c not in grid:
-            grid.append(c)
-    return grid
 def playfair_segment(message):
     return textwrap.wrap(message, width=2)
-def playfair_grid_row_column(letter, grid):
-    letter_index = grid.index(letter)
-    return letter_index // 5, letter_index % 5
-def playfair_grid_get(row, column, grid):
-    row = row % 5
-    column = column % 5
-    return grid[row*5 + column]
 def playfair_translate_e(segment, grid):
     # letter 0 row, letter 0 column
-    l_0_r, l_0_c = playfair_grid_row_column(segment[0], grid)
+    l_0_r, l_0_c = grid.row_column(segment[0])
     if len(segment) == 1 or segment[0] == segment[1]:
-        l_1_r, l_1_c = playfair_grid_row_column("x", grid)
+        l_1_r, l_1_c = grid.row_column("x")
     else:
-        l_1_r, l_1_c = playfair_grid_row_column(segment[1], grid)
+        l_1_r, l_1_c = grid.row_column(segment[1])
     # t_0 = translated 0 letter
     if l_0_r == l_1_r:
-        t_0 = playfair_grid_get(l_0_r, l_0_c + 1, grid)
-        t_1 = playfair_grid_get(l_1_r, l_1_c + 1, grid)
+        t_0 = grid.get(l_0_r, l_0_c + 1)
+        t_1 = grid.get(l_1_r, l_1_c + 1)
     elif l_0_c == l_1_c:
-        t_0 = playfair_grid_get(l_0_r + 1, l_0_c, grid)
-        t_1 = playfair_grid_get(l_1_r + 1, l_1_c, grid)
+        t_0 = grid.get(l_0_r + 1, l_0_c)
+        t_1 = grid.get(l_1_r + 1, l_1_c)
     else:
-        t_0 = playfair_grid_get(l_0_r, l_1_c, grid)
-        t_1 = playfair_grid_get(l_1_r, l_0_c, grid)
+        t_0 = grid.get(l_0_r, l_1_c)
+        t_1 = grid.get(l_1_r, l_0_c)
     return t_0 + t_1
 def playfair_translate_d(segment, grid):
     # letter 0 row, letter 0 column
-    l_0_r, l_0_c = playfair_grid_row_column(segment[0], grid)
-    l_1_r, l_1_c = playfair_grid_row_column(segment[1], grid)
+    l_0_r, l_0_c = grid.row_column(segment[0])
+    l_1_r, l_1_c = grid.row_column(segment[1])
     # t_0 = translated 0 letter
     if l_0_r == l_1_r:
-        t_0 = playfair_grid_get(l_0_r, l_0_c - 1, grid)
-        t_1 = playfair_grid_get(l_1_r, l_1_c - 1, grid)
+        t_0 = grid.get(l_0_r, l_0_c - 1)
+        t_1 = grid.get(l_1_r, l_1_c - 1)
     elif l_0_c == l_1_c:
-        t_0 = playfair_grid_get(l_0_r - 1, l_0_c, grid)
-        t_1 = playfair_grid_get(l_1_r - 1, l_1_c, grid)
+        t_0 = grid.get(l_0_r - 1, l_0_c)
+        t_1 = grid.get(l_1_r - 1, l_1_c)
     else:
-        t_0 = playfair_grid_get(l_0_r, l_1_c, grid)
-        t_1 = playfair_grid_get(l_1_r, l_0_c, grid)
+        t_0 = grid.get(l_0_r, l_1_c)
+        t_1 = grid.get(l_1_r, l_0_c)
     return t_0 + t_1
 def playfair_e(codeword, d_message):
-    grid = playfair_grid(codeword)
+    grid = Polybius(codeword)
     e_message = ""
     for segment in playfair_segment(d_message):
         e_message += playfair_translate_e(segment, grid)
     return e_message
 def playfair_d(codeword, e_message):
-    grid = playfair_grid(codeword)
+    grid = Polybius(codeword)
     d_message = ""
     for segment in playfair_segment(e_message):
         d_message += playfair_translate_d(segment, grid)
--- ww1_ciphers/tools.py
+++ ww1_ciphers/tools.py
@@ -1,3 +1,4 @@
+import string
 def clean_text(text):
     return ''.join(char for char in text.lower() if char.isalpha()).replace('j','i')
 def handle_input(input):
@@ -6,3 +7,22 @@ def handle_input(input):
             return f.read()
     except:
         return input
+class Polybius:
+    def __init__(self, codeword):
+        codeword = clean_text(codeword)
+        self.grid = []
+        for c in codeword:
+            if c not in self.grid:
+                self.grid.append(c)
+        for c in string.ascii_lowercase:
+            if c == 'j':
+                continue
+            if c not in self.grid:
+                self.grid.append(c)
+    def row_column(self,letter):
+        letter_index = self.grid.index(letter)
+        return letter_index // 5, letter_index % 5
+    def get(self, row, column):
+            row = row % 5
+            column = column % 5
+            return self.grid[row*5 + column]
